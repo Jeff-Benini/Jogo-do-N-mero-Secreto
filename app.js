c1a1897eeb308866e7a42e2822baa72dfbf985ed
Jéferson Benini <jefersonbenini@gmail.com>
2026-01-24 08:02:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensageminicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 1000');
 }
 
 exibirMensageminicial();
@@ -66,6 +66,7 @@ function reiniciarJogo() {
     exibirMensageminicial();
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
+
 
 
 
